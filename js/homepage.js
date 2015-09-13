c82b70344cd98baf5fb149e4f8c3fd1a58d84cb7
Finn Lawrence <finn.lawrence@gmail.com>
2015-09-13 21:06:17 +1200

    Implemented portrait images, contact form

--- js/homepage.js
+++ js/homepage.js
@@ -1,5 +1,20 @@
 $(document).ready(function() { 
    $('.parallax').parallax();
+    aspect();
+    
+    $('a[href^=#]').click(function(){
+        if ($.attr(this, 'href') === "#" || $(this).hasClass("ignore-scroll")) {
+            if ($(this).data("target") != null) {
+                var target = $( $(this).data("target") ).offset().top - $('.control-buttons').height();
+                $('html, body').animate({
+                    scrollTop: target
+                }, 600);
+                return false;
+            } else {
+                return true;
+            }
+        }
+    });
 });
 
 $(window).load(function() {
@@ -7,10 +22,22 @@ $(window).load(function() {
 });
 
 $(window).resize(function() {
-    
+    aspect();
 });
 
 // Scrolling
 $(window).scroll(function() {
     
 });
+
+function aspect() {
+    var aspect = $(window).width() / $(window).height();
+
+    if (aspect > 1) {
+        $('.landscape').css("display","block");
+        $('.portrait').css("display","none");
+    } else {
+        $('.landscape').css("display","none");
+        $('.portrait').css("display","block");
+    }
+}
