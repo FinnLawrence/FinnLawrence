c2d70e8947ffcd6ebc37bf2b3c23dac7a02fad94
Finn Lawrence <finn.lawrence@gmail.com>
2015-09-13 16:47:40 +1200

    Initial Commit

---
+++ js/homepage.js
@@ -0,0 +1,16 @@
+$(document).ready(function() { 
+   $('.parallax').parallax();
+});
+
+$(window).load(function() {
+    
+});
+
+$(window).resize(function() {
+    
+});
+
+// Scrolling
+$(window).scroll(function() {
+    
+});
