0f0fcfb41a50fbb9554c6b9bb1079b9e72a6832c
Finn Lawrence <finn.lawrence@gmail.com>
2015-09-23 20:56:45 +1200

    Added analytics hooks to collapsibles

--- js/homepage.js
+++ js/homepage.js
@@ -1,8 +1,13 @@
-$(document).ready(function() { 
+$(document).ready(function() {
+    
+    // Fire up the parallax
    $('.parallax').parallax();
+    
+    // Display correct elements for current aspect ratio
     aspect();
     
-    $('a[href^=#]').click(function(){
+    // Smooth scrolling
+    $('a[href^=#]').click(function() {
         if ($.attr(this, 'href') === "#" || $(this).hasClass("ignore-scroll")) {
             if ($(this).data("target") != null) {
                 var target = $( $(this).data("target") ).offset().top - $('.control-buttons').height();
@@ -15,28 +20,38 @@ $(document).ready(function() {
             }
         }
     });
-});
-
-$(window).load(function() {
+    
+    // ########
+    // GA Hooks
+    // ########
+    
+    // Open / close work experience sections
+    $('#work-current').click(function() {
+        ga('send', 'event', 'section', 'click', 'work-current');
+    });
+    $('#work-previous').click(function() {
+        ga('send', 'event', 'section', 'click', 'work-previous');
+    });
     
 });
 
+// Call aspect on window resize
 $(window).resize(function() {
     aspect();
 });
 
-// Scrolling
-$(window).scroll(function() {
-    
-});
-
+// Calculates the aspect ratio and hides/shows elements with .landscape / .portrait classes appropriately
 function aspect() {
+    
+    // Calculate window aspect ratio
     var aspect = $(window).width() / $(window).height();
 
     if (aspect > 1) {
+        // We're in landscape view
         $('.landscape').css("display","block");
         $('.portrait').css("display","none");
     } else {
+        // We're in portrait view
         $('.landscape').css("display","none");
         $('.portrait').css("display","block");
     }
