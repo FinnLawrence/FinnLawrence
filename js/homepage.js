5872767c5f7a5e178fad088aa5cc79d702d30bff
Finn Lawrence <finn.lawrence@gmail.com>
2016-05-23 23:29:23 +1200

    Removed collapsibles

--- js/homepage.js
+++ js/homepage.js
@@ -1,38 +1,34 @@
 $(document).ready(function() {
-    
+    // Parse any parameters
+    parseParameters();
+
     // Fire up the parallax
-   $('.parallax').parallax();
-    
+    $('.parallax').parallax();
+
     // Display correct elements for current aspect ratio
     aspect();
-    
+
+    // Handlers
+    $('#modal-message-submitted .modal-close').click(function() {
+        history.pushState(null, null, window.location.href.substring(0, window.location.href.indexOf('?')));
+    });
+
     // Smooth scrolling
     $('a[href^=#]').click(function() {
         if ($.attr(this, 'href') === "#" || $(this).hasClass("ignore-scroll")) {
             if ($(this).data("target") != null) {
-                var target = $( $(this).data("target") ).offset().top - $('.control-buttons').height();
+                var current = $(window).scrollTop();
+                var target = $( $(this).data("target") ).offset().top;
+                var duration = Math.abs(target - current);
                 $('html, body').animate({
                     scrollTop: target
-                }, 600);
+                }, duration);
                 return false;
             } else {
                 return true;
             }
         }
     });
-    
-    // ########
-    // GA Hooks
-    // ########
-    
-    // Open / close work experience sections
-    $('#work-current').click(function() {
-        ga('send', 'event', 'section', 'click', 'work-current');
-    });
-    $('#work-previous').click(function() {
-        ga('send', 'event', 'section', 'click', 'work-previous');
-    });
-    
 });
 
 // Call aspect on window resize
@@ -42,7 +38,7 @@ $(window).resize(function() {
 
 // Calculates the aspect ratio and hides/shows elements with .landscape / .portrait classes appropriately
 function aspect() {
-    
+
     // Calculate window aspect ratio
     var aspect = $(window).width() / $(window).height();
 
@@ -54,5 +50,15 @@ function aspect() {
         // We're in portrait view
         $('.landscape').css("display","none");
         $('.portrait').css("display","block");
+    }
+}
+
+// Parses URL parameters and does stuff if they're there
+function parseParameters() {
+    var pageURL = window.location.href;
+    var message = url('?message', pageURL);
+
+    if (message == "submitted") {
+        $('#modal-message-submitted').openModal();
     }
 }
